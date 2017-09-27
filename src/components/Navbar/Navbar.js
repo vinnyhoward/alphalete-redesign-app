fbf59d8161450c2b358e94a61b5785f16d97826c
Vincent Howard <vincenthoward@Vincents-MacBook-Pro.local>
2017-09-27 10:25:51 -0700

    Dropdown menu v1

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,40 @@ import { Link } from "react-router-dom";
 
 
 class Navbar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      slideVisible: false
+    }
+this.openSlide = this.openSlide.bind(this)
+  }
+
+openSlide() {
+  this.setState({
+    slideVisible: !this.state.slideVisible
+  })
+}
+
+
   render() {
     return (
       <div className="Navbar">
       <ul className="Navbar-Container">
         <div className="Nav-padding">
         <li><Link to="/mens">MEN'S</Link></li>
+        <button
+          className='btn'
+          onMouseOver={ this.openSlide }
+          >MENU</button>
         <li><Link to="/womens">WOMEN'S</Link></li>
         <li><Link to="/accessories">ACCESSORIES</Link></li>
-        {/* <li className="Main-logo"><a>Logo</a></li> */}
+        {/* <li><img className="Main-logo" src='' /></li> */}
         <li className="Nav-right"><Link to="/cart">CART</Link></li>
         <li className="Nav-right"><Link to="/login">ACCOUNT</Link></li>
         <li className="Nav-right"><a>SEARCH</a></li>
         </div>
       </ul>
+      <div className={this.state.slideVisible ? 'slide open' : 'slide'}></div>
     </div>
     );
   }
